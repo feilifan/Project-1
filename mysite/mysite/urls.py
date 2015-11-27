f06d9d607142d8f90ce3c06ac9361183b6d257d2
caichanglong <834819956@qq.com>
2015-11-27 11:48:06 +0800

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from addr_book.views import class_apply,class_binfo,class_discuss,class_home,class_admin,class_delete
-from addr_book.views import class_mainlist,class_register,class_seediscuss,class_selflist
+from addr_book.views import class_mainlist,class_register,class_seediscuss,class_selflist, class_check
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
     (r'^register/',class_register),
     (r'^class_seediscuss/',class_seediscuss),
     (r'^class_selflist/',class_selflist),
+    (r'^class_check/',class_check),
     (r'^site_media/(?P<path>.*)','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
     (r'^css/(?P<path>.*)','django.views.static.serve',{'document_root':settings.STATIC_ROOT,}),
 )
